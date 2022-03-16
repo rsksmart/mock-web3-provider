0d9a0b70e9833de2cc00082b23542d0746d45ed7
Ilan <ilanolkies@outlook.com>
2022-03-16 19:11:44 -0300

    Refactor: make mock provider a class

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,11 @@
-import mockProvider from './index'
+import { MockProvider } from './index'
 
 describe('provider', () => {
   const address = '0xB98bD7C7f656290071E52D1aA617D9cB4467Fd6D'
   const privateKey = 'de926db3012af759b4f24b5a51ef6afa397f04670f634aa4f48d4480417007f3'
 
-  const provider = mockProvider({
-    address, privateKey, chainId: 31, debug: false
+  const provider = new MockProvider({
+    address, privateKey, networkVersion: 31, debug: false
   })
 
   it('returns a provider object', () => {
--- src/index.ts
+++ src/index.ts
@@ -3,79 +3,94 @@ import { personalSign, decrypt } from 'eth-sig-util'
 interface ProviderSetup {
   address: string,
   privateKey: string,
-  chainId: number,
+  networkVersion: number,
   debug?: boolean
+  manualConfirmEnable?: boolean
 }
 
-const provider = (startProps: ProviderSetup) => {
-  const {
-    address, privateKey, chainId, debug
-  } = startProps
+export class MockProvider {
+  setup: ProviderSetup
 
-  /* Logging */
-  // eslint-disable-next-line no-console
-  const log = (...args: (any | null)[]) => debug && console.log('🦄', ...args)
-
-  const buildProvider = {
-    isMetaMask: true,
-    networkVersion: chainId,
-    chainId: `0x${chainId.toString(16)}`,
-    selectedAddress: address,
-
-    request(props: { method: any; params: string[] }) {
-      log(`request[${props.method}]`)
-      switch (props.method) {
-        case 'eth_requestAccounts':
-        case 'eth_accounts':
-          return Promise.resolve([this.selectedAddress])
-        case 'net_version':
-          return Promise.resolve(this.networkVersion)
-        case 'eth_chainId':
-          return Promise.resolve(this.chainId)
-
-        case 'personal_sign': {
-          const privKey = Buffer.from(privateKey, 'hex');
-          const signed = personalSign(privKey, { data: props.params[0] })
-          log('signed', signed)
-          return Promise.resolve(signed)
-        }
-        case 'eth_sendTransaction': {
-          return Promise.reject(new Error('This service can not send transactions.'))
-        }
-        case 'eth_decrypt': {
-          log('eth_decrypt', props)
-          const stripped = props.params[0].substring(2)
-          const buff = Buffer.from(stripped, 'hex');
-          const data = JSON.parse(buff.toString('utf8'));
-          return Promise.resolve(decrypt(data, privateKey))
-        }
-        default:
-          log(`resquesting missing method ${props.method}`)
-          // eslint-disable-next-line prefer-promise-reject-errors
-          return Promise.reject(`The method ${props.method} is not implemented by the mock provider.`)
-      }
-    },
-
-    sendAsync(props: { method: string }, cb: any) {
-      switch (props.method) {
-        case 'eth_accounts':
-          cb(null, { result: [this.selectedAddress] })
-          break;
-        case 'net_version': cb(null, { result: this.networkVersion })
-          break;
-        default: log(`Method '${props.method}' is not supported yet.`)
-      }
-    },
-    on(props: string) {
-      log('registering event:', props)
-    },
-    removeAllListeners() {
-      log('removeAllListeners', null)
-    },
+  constructor(setup: ProviderSetup) {
+    this.setup = setup
   }
 
-  log('Mock Provider ', buildProvider)
-  return buildProvider;
-}
+  private log = (...args: (any | null)[]) => this.setup.debug && console.log('🦄', ...args)
 
-export default provider
+  get selectedAddress() {
+    return this.setup.address
+  }
+
+  get networkVersion() {
+    return this.setup.networkVersion
+  }
+
+  get chainId() {
+    return `0x${this.setup.networkVersion.toString(16)}`
+  }
+
+  request(props: { method: any; params: string[] }) {
+    this.log(`request[${props.method}]`)
+
+    switch (props.method) {
+      case 'eth_requestAccounts':
+      case 'eth_accounts':
+        return Promise.resolve([this.setup.address])
+
+      case 'net_version':
+        return Promise.resolve(this.setup.networkVersion)
+
+      case 'eth_chainId':
+        return Promise.resolve(this.chainId)
+
+      case 'personal_sign': {
+        const privKey = Buffer.from(this.setup.privateKey, 'hex');
+        const signed = personalSign(privKey, { data: props.params[0] })
+
+        this.log('signed', signed)
+
+        return Promise.resolve(signed)
+      }
+
+      case 'eth_sendTransaction': {
+        return Promise.reject(new Error('This service can not send transactions.'))
+      }
+
+      case 'eth_decrypt': {
+        this.log('eth_decrypt', props)
+
+        const stripped = props.params[0].substring(2)
+        const buff = Buffer.from(stripped, 'hex');
+        const data = JSON.parse(buff.toString('utf8'));
+
+        return Promise.resolve(decrypt(data, this.setup.privateKey))
+      }
+
+      default:
+        this.log(`resquesting missing method ${props.method}`)
+        // eslint-disable-next-line prefer-promise-reject-errors
+        return Promise.reject(`The method ${props.method} is not implemented by the mock provider.`)
+    }
+  }
+
+  sendAsync(props: { method: string }, cb: any) {
+    switch (props.method) {
+      case 'eth_accounts':
+        cb(null, { result: [this.setup.address] })
+        break;
+
+      case 'net_version': cb(null, { result: this.setup.networkVersion })
+        break;
+
+      default: this.log(`Method '${props.method}' is not supported yet.`)
+    }
+  }
+
+  on(props: string) {
+    this.log('registering event:', props)
+  }
+
+  removeAllListeners() {
+    this.log('removeAllListeners', null)
+  }
+}
